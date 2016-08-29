6353abab125ec5355cb632586ea1dbb741ceab82
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-08-28 20:42:42 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,46 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+  it('should filter for favorite food', function(){
+	  var fakePeople = [
+	  	{
+	  		name: 'Harvey Hiddleston',
+	  		favoriteFood: 'Guacamole'
+	  	},
+	  	{
+	  		name: 'Henrietta Hogson',
+	  		favoriteFood: 'Guacamole'
+	  	},
+	  	{
+	  		name: 'No one',
+	  		favoriteFood: 'Guacamole'
+	  	},
+	  	{
+	  		name: 'Hugo Bandweaver',
+	  		favoriteFood: 'Fiddlesticks'
+	  	},
+	  	{
+	  		name: 'Severus Seamsworth',
+	  		favoriteFood: 'Fish sticks'
+	  	},
+	  	{
+	  		name: 'Stone Parkwell',
+	  		favoriteFood: 'Fish sticks'
+	  	}
+	  ];
+
+	  var favoritePeople = $filter('favoriteFood')(fakePeople, 'Fish sticks');
+	  expect(favoritePeople.length).toEqual(2);
+
+	  var goodPeople = $filter('favoriteFood')(fakePeople, 'Guacamole');
+	  expect(goodPeople.length).toEqual(3);
+	  expect(goodPeople[2].name).toEqual('No one');
+
+	  var trollPeople = $filter('favoriteFood')(fakePeople, 'Fiddlesticks');
+	  expect(trollPeople.length).toEqual(1);		  
+  });
+
+
+
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,11 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should remove all vowels from a string', function(){
+		var fakeString = "There is no string";
+
+		var filteredString = $filter('removeAllVowels')(fakeString);
+
+		expect(filteredString).toEqual('Thr s n strng');
+	});
 });
